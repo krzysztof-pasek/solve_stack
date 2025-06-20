3c299476c2e2c9836cb68a200947d188caa204c1
AdamullaOsas <kpasek22@gmail.com>
2025-06-20 17:05:32 +0200

    setup and assets

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,12 @@
 import React from "react";
 
 const Home = () => {
-    return <div className="text-blue-300">Home</div>;
+    return (
+        <div className="h-full w-full flex flex-col items-center justify-center bg-blue-300">
+            <h1 className="text-amber-600 font-space-grotesk">hi</h1>
+            <h1 className="color-light-800 ">hi</h1>
+        </div>
+    );
 };
 
 export default Home;
